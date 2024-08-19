97fe4230693b83a9f40f9dcdb5069166817014c6
Brian <brianhilsden@gmail.com>
2024-08-19 11:00:30 +0300

    updated api link

--- src/components/ClerksPage.js
+++ src/components/ClerksPage.js
@@ -61,7 +61,7 @@ const ClerksPage = () => {
   const handleConfirmPackage = () => {
  ;
 
-    fetch(`http://127.0.0.1:5555/requests/${store_id}`,{
+    fetch(`https://my-duka-back-end.vercel.app/requests/${store_id}`,{
       method:"POST",
       headers:{
         "Content-Type":"application/json"
